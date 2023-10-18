0f62227ef8ef0933bf52c243a0fbeb96ab8a57b7
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-18 10:58:31 +0530

    pattern_reverse_digit_triangle

--- pattern_program/09pattern.cpp
+++ pattern_program/09pattern.cpp
@@ -5,12 +5,21 @@ int main(){
     i=1;
     cout<<"Enter the value of n : ";
     cin>>n;
+    // while(i<=n){
+    //     j=1;
+    //     int val=i;
+    //     while(j<=i){
+    //         cout<<val<<" ";
+    //         val-=1;
+    //         j+=1;
+    //     }
+    //     cout<<endl;
+    //     i+=1;
+    // }
     while(i<=n){
         j=1;
-        int val=i;
         while(j<=i){
-            cout<<val<<" ";
-            val-=1;
+            cout<<(i-j+1)<<" ";
             j+=1;
         }
         cout<<endl;
