e8e2f25ff1e79c3905b1dae63d8a6fdc0897263a
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-18 23:09:41 +0530

    triangular_char_pattern
    A
    B C
    C D E
    D E F G
    E F G H I

---
+++ pattern_program/14pattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    i=1;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    while(i<=n){
+        j=1;
+        char ch=('A'+i-1); 
+        while(j<=i){
+            // char ch=('A'+i-1); 
+            cout<<ch<<" ";
+            ch+=1;
+            j+=1;
+
+        }
+        cout<<endl;
+        
+        i+=1;
+       
+    }
+}
