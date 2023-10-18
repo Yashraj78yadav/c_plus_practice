9d9c6c801fdd9c4f68ea0d3e9ba0bcca3925535a
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-18 22:34:00 +0530

    charector_increasing_square_pattern

--- pattern_program/11pattern.cpp
+++ pattern_program/11pattern.cpp
@@ -1,22 +1,25 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int i,j,n;
-    char ch =65;
-    cout<<"Enter the value of n :";
-    cin>>n;
-    i=1;
-    while(i<=n){
-        j=1;
-         
-        while(j<=n){
-             
-            cout<<ch<<" ";
-            ch+=1;
-            j+=1;
+int main()
+{
+    int i, j, n;
+    char ch = 65;
+    cout << "Enter the value of n :";
+    cin >> n;
+    i = 1;
+    while (i <= n)
+    {
+        j = 1;
+
+        while (j <= n)
+        {
+
+            cout << ch << " ";
+            ch += 1;
+            j += 1;
         }
-        cout<<endl;
-        
-        i+=1;
+        cout << endl;
+
+        i += 1;
     }
 }
