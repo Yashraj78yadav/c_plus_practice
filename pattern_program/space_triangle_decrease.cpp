65bf16c763e85feaaa303d2cdc8224facc3c13ba
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-20 22:07:23 +0530

    tspace and triangle pattern

---
+++ pattern_program/space_triangle_decrease.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    i=1;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    while(i<=n){
+        int space=(n-i+1);
+        while(space){
+            cout<<"  ";
+            space+=1;
+        }
+        j=1;
+        while(j<=i){
+            cout<<j<<" ";
+            j+=1;
+        }
+        cout<<endl;
+        i+=1;
+    }
+
+}
