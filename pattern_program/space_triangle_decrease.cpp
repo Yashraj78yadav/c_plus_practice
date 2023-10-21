24d1cdf2e66ea50f9baeb19875b5df52fb6d4857
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-21 10:07:20 +0530

    space triangle and deccrease triangle
              1
            1 2 1
          1 2 3 2 1
        1 2 3 4 3 2 1
      1 2 3 4 5 4 3 2 1

--- pattern_program/space_triangle_decrease.cpp
+++ pattern_program/space_triangle_decrease.cpp
@@ -9,12 +9,17 @@ int main(){
         int space=(n-i+1);
         while(space){
             cout<<"  ";
-            space+=1;
+            space-=1;
         }
         j=1;
         while(j<=i){
             cout<<j<<" ";
             j+=1;
+        }
+        int end=i-1;
+        while(end){
+            cout<<end<<" ";
+            end-=1;
         }
         cout<<endl;
         i+=1;
