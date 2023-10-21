552882a864ae3c7dc8a7a684b7e2333eb9a81c80
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-21 19:29:01 +0530

    space triangle pattern

--- pattern_program/space_triangle_decrease.cpp
+++ pattern_program/space_triangle_decrease.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int i,j,n;
-    i=1;
-    cout<<"Enter the value of n: ";
-    cin>>n;
-    while(i<=n){
-        int space=(n-i+1);
-        while(space){
-            cout<<"  ";
-            space-=1;
+int main()
+{
+    int i, j, n;
+    i = 1;
+    cout << "Enter the value of n: ";
+    cin >> n;
+    while (i <= n)
+    {
+        int space = (n - i + 1);
+        while (space)
+        {
+            cout << "  ";
+            space -= 1;
         }
-        j=1;
-        while(j<=i){
-            cout<<j<<" ";
-            j+=1;
+        j = 1;
+        while (j <= i)
+        {
+            cout << j << " ";
+            j += 1;
         }
-        int end=i-1;
-        while(end){
-            cout<<end<<" ";
-            end-=1;
+        int end = i - 1;
+        while (end)
+        {
+            cout << end << " ";
+            end -= 1;
         }
-        cout<<endl;
-        i+=1;
+        cout << endl;
+        i += 1;
     }
-
 }
