9e9a8576a0d3a023c51b39c454f3377312635d4b
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-22 22:59:07 +0530

    bitwise perform program checking
    Enter the value of a: 5
    Enter the value of b: 6
    The and of a b(a & b) is : 4
    The or of a b(a | b) is : 7
    The not of a b( ~a) is : -6
    The not of a b( ~b) is : -7
    The xor of a b(a ^ b) is : 3

---
+++ bitwise_operator/bitwise_perform.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+    cout<<"The and of a b(a & b) is : "<<(a&b)<<endl;
+    cout<<"The or of a b(a | b) is : "<<(a|b)<<endl;
+    cout<<"The not of a b( ~a) is : "<<(~a)<<endl;
+    cout<<"The not of a b( ~b) is : "<<(~b)<<endl;
+    cout<<"The xor of a b(a ^ b) is : "<<(a^b)<<endl;
+
+}
