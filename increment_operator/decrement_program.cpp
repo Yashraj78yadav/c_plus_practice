e267cea20dc862a7b208b9ae6ad68358d2bacbc2
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-23 23:12:55 +0530

    decrement operator program
    Enter the value of number :5
    The post decrement (num--) is :5 after decrement 4
    The pre decrement (--num) is :3 after decrement 3

---
+++ increment_operator/decrement_program.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+cout<<"Enter the value of number :";
+cin>>num;
+cout<<"The post decrement (num--) is :"<<(num--)<<" "<<"after decrement"<<" "<<num<<endl;
+cout<<"The pre decrement (--num) is :"<<(--num)<<" "<<"after decrement"<<" "<<num;
+}
