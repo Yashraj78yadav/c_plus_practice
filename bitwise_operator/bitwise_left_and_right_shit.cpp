6ed42e35a7f44410c6f54db7ca40f1cfc965001b
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-23 22:55:02 +0530

    left and right shift bitwise operator
     Enter the value of a: 5
    Enter the value of b: 4
    The right shift of a and  1(a>>b)is : 2
    Thee left shift if a and b(a<<b) is: 16

---
+++ bitwise_operator/bitwise_left_and_right_shit.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<" Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+    cout<<"The right shift of a and  1(a>>b)is : "<<(a>>1)<<endl;
+    cout<<"Thee left shift if a and b(a<<b) is: "<<(b<<2);
+}
