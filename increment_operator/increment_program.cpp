d65cb4fd88ad1a9fdbf6062a43e3c15a3b4c9656
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-23 23:09:24 +0530

    increment operator
    Enter the value of number :5
    The post increment (num++) is :5 after increment 6
    The pre increment (++num) is :7 after increment 7

---
+++ increment_operator/increment_program.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+int num;
+cout<<"Enter the value of number :";
+cin>>num;
+cout<<"The post increment (num++) is :"<<(num++)<<" "<<"after increment"<<" "<<num<<endl;
+cout<<"The pre increment (++num) is :"<<(++num)<<" "<<"after increment"<<" "<<num;
+}
