59acc1e80918ed93478d5286901f2bca3dacbd56
Yashraj78yadav <richyashrajyadav@gmail.com>
2023-10-24 15:20:38 +0530

    continue statement program
    I am yashraj yadav
    I am yashraj yadav
    I am yashraj yadav
    I am yashraj yadav
    I am yashraj yadav

---
+++ prime_number/continue_statement_program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<"I am ";
+        cout<<"yashraj yadav"<<endl;
+        continue;
+        cout<<" I am fresher"<<endl;
+    }
+    
+}
