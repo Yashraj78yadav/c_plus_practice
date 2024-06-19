d1f4f48531faf4cae2603535710fa8739bc5058b
Yashraj Yadav <146312282+Yashraj78yadav@users.noreply.github.com>
2024-06-19 16:49:08 +0530

    full_hallow_pyramid

---
+++ pattern_program/full_hallow_pyramid.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n\n ";
+    cin>>n;
+    for(int row=0;row<n;row++){
+        for(int col1=0;col1<(n-row-1);col1++){
+            cout<<"$";
+        }
+        for(int col2=0;col2<row+1;col2++){
+            if(row==0||row==n-1||col2==0||col2==row+2){
+                cout<<"* ";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
