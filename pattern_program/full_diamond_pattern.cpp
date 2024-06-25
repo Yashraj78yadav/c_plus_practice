a93688a0454ff6ba4a7ab5dc6f6dcf36443955c2
Yashraj Yadav <146312282+Yashraj78yadav@users.noreply.github.com>
2024-06-25 16:13:47 +0530

    full_diamond_pattern
    output are
    Enter the value of n : 6
         *
        * *
       * * *
      * * * *
     * * * * *
    * * * * * *
    * * * * * *
     * * * * *
      * * * *
       * * *
        * *
         *

---
+++ pattern_program/full_diamond_pattern.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n 
+    ;
+    cout<<"Enter the value of n : ";
+    cin>>n;
+    for(int row=0;row<n;row++){
+        for(int col1=0;col1<n-row-1;col1++){
+            cout<<" ";
+        }
+        for(int col2=0;col2<row+1;col2++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int row=0;row<n;row++){
+        for(int col1=0;col1<row;col1++){
+            cout<<" ";
+        }
+        for(int col2=0;col2<n-row;col2++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
