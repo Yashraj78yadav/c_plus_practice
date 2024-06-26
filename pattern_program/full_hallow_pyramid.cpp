3ae9f0ba7a2f85a4d6d6a9b95356159ca47b464a
Yashraj Yadav <146312282+Yashraj78yadav@users.noreply.github.com>
2024-06-26 09:04:41 +0530

    full_hallow_pyramid
    output
    Enter the value of n
     6
              *
            *    *
          *          *
        *               *
      *                    *
    * * * * * * * * * * *

--- pattern_program/full_hallow_pyramid.cpp
+++ pattern_program/full_hallow_pyramid.cpp
@@ -6,14 +6,14 @@ int main(){
     cin>>n;
     for(int row=0;row<n;row++){
         for(int col1=0;col1<(n-row-1);col1++){
-            cout<<"$";
+            cout<<"  ";
         }
-        for(int col2=0;col2<row+1;col2++){
-            if(row==0||row==n-1||col2==0||col2==row+1){
+        for(int col2=0;col2<2*row+1;col2++){
+            if(row==n-1||col2==0||col2==2*row){
                 cout<<"* ";
             }
             else{
-                cout<<" ";
+                cout<<"  ";
             }
         }
         cout<<endl;
