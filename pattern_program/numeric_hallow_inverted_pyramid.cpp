2e18cdf1f89652ff93479864029fbf9a1d17d7e5
Yashraj Yadav <146312282+Yashraj78yadav@users.noreply.github.com>
2024-06-29 16:44:56 +0530

    numeric hallow inverted pyramid pattern
    output
    Enter the value of n :6
    1 2 3 4 5
    2     5
    3   5
    4 5
    5

--- pattern_program/numeric_hallow_inverted_pyramid.cpp
+++ pattern_program/numeric_hallow_inverted_pyramid.cpp
@@ -4,16 +4,17 @@ int main(){
     int n;
     cout<<"Enter the value of n :";
     cin>>n;
-    for(int row=0;row<n;row++){
-            int num=1;
+    int row;
+    for( row=0;row<n;row++){
+            int num=row;
         for(int col=0;col<n-row-1;col++){
-            if(row==0||col==0||col==n-row-2){
-                cout<<num;
                 num++;
+            if(row==0||col==0||col==n-row-2){
+                cout<<num<<" ";
                 
             }
             else{
-                cout<<" ";
+                cout<<"  ";
             }
            
         }
