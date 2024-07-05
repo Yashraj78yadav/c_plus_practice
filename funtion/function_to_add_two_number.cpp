3e4bf905de6178b0954c063e416851e268608d7a
Yashraj Yadav <146312282+Yashraj78yadav@users.noreply.github.com>
2024-07-05 10:47:38 +0530

    function to add number
    output
    Enter the value of a and b respectively: 5 6
    Ethe sum of a and b is 11

---
+++ funtion/function_to_add_two_number.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+void addNumber(){
+    int a,b;
+    cout<<"Enter the value of a and b respectively: ";
+    cin>>a>>b;
+    cout<<"Ethe sum of a and b is "<<a+b;
+}
+int main(){
+    addNumber();
+    return 0;
+}
